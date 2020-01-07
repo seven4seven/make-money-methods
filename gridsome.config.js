a833ba656a4a6b65bade21b171cc62e3b7bfd583
seven4seven <59610806+seven4seven@users.noreply.github.com>
2020-01-07 16:23:24 +0100

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: 'Money Making Methods 2020',
+  siteDescription: 'Rock on 2020 with deam simple Money Making Methods',
 
   templates: {
     Post: '/:title',
